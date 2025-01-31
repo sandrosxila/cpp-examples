94a8323255d2f469f4f1ef1272d4c2eba7259b72
sandrosxila <sandrosxila@yahoo.com>
2025-01-31 17:26:45 +0400

    reorganize

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,41 +1,6 @@
 #include <bits/stdc++.h>
+#include "dsu.cpp"
 using namespace std;
-
-class DSU {
-    private:
-    vector<int> parent, rank;
-    public:
-    DSU(int n) {
-        parent = vector(n, 0);
-        rank = vector(n, 0);
-        
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-    
-    int find(int v) {
-        if (v == parent[v])
-            return v;
-        return parent[v] = find(parent[v]);
-    }
-    
-    void merge(int x, int y) {
-        int a = find(x);
-        int b = find(y);
-        
-        if(a != b){  
-            if(rank[a] < rank[b])
-                swap(a, b);
-            
-            parent[b] = a;
-            
-            if(rank[a] == rank[b])
-                rank[a]++;
-        }
-    }
-};
 
 struct Edge {
     int from; 
@@ -90,3 +55,8 @@ int main() {
 
   return 0;
 }
+
+/*
+* run in powershell:
+* g++ ./kruskal.cpp -o ./output/kruskal; ./output/kruskal
+*/
